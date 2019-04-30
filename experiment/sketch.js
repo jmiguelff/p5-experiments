d6db80cddc369f3cf1f5c90c3dbb8e72f4de7066
Joao Ferreira <joao.ferreira@nomadtech.pt>
2019-04-30 11:45:14 +0100

    - Load sound with volume control

--- experiment/sketch.js
+++ experiment/sketch.js
@@ -1,7 +1,19 @@
+var song;
+var slider;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
+  song = loadSound("assets/samlemp3.mp3", loaded);
+  slider = createSlider(0, 1, 0.5, 0.01);
+  slider.position(20, 20);
+}
+
+function loaded() {
+  song.play();
+  console.log("play");
 }
 
 function draw() {
-
+  background(255, 0, 100);
+  song.setVolume(slider.value());
 }
